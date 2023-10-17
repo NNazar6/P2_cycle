f883905286adafbebf96e23131b0e9e86efd6ede
NNazar6 <144454064+NNazar6@users.noreply.github.com>
2023-10-17 13:02:00 +0300

    Add files via upload

---
+++ A_2_cycle.py
@@ -0,0 +1,62 @@
+# a = int(input('Enter: '))
+# b = int(input('Enter: '))
+# if a<=b:
+#     for i in range(a, b+1):
+#         print(i)
+# else:
+#     print('Condition aren`t met')
+
+# ===================================================================02
+
+# a = int(input('Enter: '))
+# b = int(input('Enter: '))
+# if a<=b:
+#     for i in range(a, b+1):
+#         print(i)
+# elif a>=b:
+#     for c in range(a, b - 1, -1):
+#         print(c)
+
+# =====================================================================03
+#
+# n = int(input('Enter: '))
+# sum = 0
+# for i in range(n):
+#     sum += int(input('Enter: '))
+#     print(sum)
+
+# ========================================================================04
+
+# n = int(input('Enter: '))
+# factorial = 1
+# for i in range(1,n+1 ):
+#     factorial *= i
+#     print(factorial)
+
+# =========================================================================05
+
+# n = int(input('Enter: '))
+# for i in range(1, n+1):
+#     for f in range(1, i+1):
+#         print(f,sep='', end='')
+#     print()
+#===========================================================================06
+
+# n = int(input('Enter: '))
+# i = 1
+# while i**2 <= n:
+#     print(i, i**2)
+#     i += 1
+
+# =========================================================================07
+
+# n = int(input('Enter: '))
+# num = 2
+# degree_indicator = 1
+# while num <= n:
+#     num *= 2
+#     degree_indicator +=1
+#     print(degree_indicator -1, num//2)
+# ========================================================================08
+x = int(input('enter: '))
+y = int(input('enter: '))
